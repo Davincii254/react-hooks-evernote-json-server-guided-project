ab7a1e1b6c75a22bb731242020a2ff4440548104
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-04-03 12:09:57 -0500

    Add function to show note when clicked

--- src/components/NoteContainer.js
+++ src/components/NoteContainer.js
@@ -6,6 +6,7 @@ import Content from "./Content";
 function NoteContainer() {
 
   const [list, setList] = useState([]);
+  const [showItem, setShowItem] = useState({});
 
   useEffect( () => {
     fetch(`http://localhost:3000/notes`)
@@ -13,12 +14,17 @@ function NoteContainer() {
     .then(data => setList(data));
   },[]);
 
+  function handleShow(title, body){
+    setShowItem({title: title, body: body});
+    //console.log(showItem);
+  }
+
   return (
     <>
       <Search />
       <div className="container">
-        <Sidebar list={list}/>
-        <Content />
+        <Sidebar list={list} handleShow={handleShow}/>
+        <Content showItem={showItem}/>
       </div>
     </>
   );
