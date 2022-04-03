8b566b4943d564bfa714e84fba7c7913c11bc2e9
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-04-03 10:48:33 -0500

    Add fetch to get data

--- src/components/NoteContainer.js
+++ src/components/NoteContainer.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Search from "./Search";
 import Sidebar from "./Sidebar";
 import Content from "./Content";
 
 function NoteContainer() {
+
+  const [list, setList] = useState([]);
+
+  useEffect( () => {
+    fetch(`http://localhost:3000/notes`)
+    .then(r => r.json())
+    .then(data => setList(data));
+  },[]);
+
   return (
     <>
       <Search />
