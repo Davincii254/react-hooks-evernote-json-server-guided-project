9cecf7860e1ce426ac4b9163fbe67119dd9c3c05
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-04-05 12:15:41 -0500

    Pass handleSave to NoteEditor

--- src/components/NoteEditor.js
+++ src/components/NoteEditor.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-function NoteEditor({editTitle, handleTitleEdit, editBody, handleBodyEdit, handleEditBtn}) {
-  
+function NoteEditor({editTitle, handleTitleEdit, editBody, handleBodyEdit, handleEditBtn, handleSave}) {
+  //console.log(showItem.id);
   return (
-    <form className="note-editor">
+    <form className="note-editor" onSubmit={handleSave}>
       <input type="text" name="title" value={editTitle} onChange={handleTitleEdit}/>
       <textarea name="body" value={editBody} onChange={handleBodyEdit}/>
       <div className="button-row">
-        <input className="button" type="submit" value="Save" />
+        <input className="button" type="submit" value="Save"/>
         <button type="button" onClick={handleEditBtn}>Cancel</button>
       </div>
     </form>
