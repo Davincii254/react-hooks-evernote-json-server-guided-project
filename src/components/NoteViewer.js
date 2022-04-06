3bf27a3646c9f326ad8bb4ff010677d4a0a419fe
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-04-06 12:39:48 -0500

    Add delete function to server

--- src/components/NoteViewer.js
+++ src/components/NoteViewer.js
@@ -1,11 +1,23 @@
 import React from "react";
 
-function NoteViewer({showItem, handleEditBtn}) {
+function NoteViewer({showItem, handleEditBtn, handleDeleteItem}) {
+
+
+  function handleDeleteClick(){
+    fetch(`http://localhost:3000/notes/${showItem.id}`, {
+      method: "DELETE",
+    })
+    .then(r => r.json())
+    .then(() => handleDeleteItem(showItem))
+    .catch(error => alert(error));
+  }
+
   return (
     <>
       <h2>{showItem.title}</h2>
       <p>{showItem.body}</p>
       <button onClick={() => handleEditBtn(showItem)}>Edit</button>
+      <button onClick={handleDeleteClick}>Delete</button>
     </>
   );
 }
