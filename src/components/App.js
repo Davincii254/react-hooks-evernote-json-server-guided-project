1bb50b9dc594d11a04df0c3451ea195969f43158
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-04-07 12:43:37 -0500

    Add state and fetch for user login

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import NoteContainer from "./NoteContainer";
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState([]);
+
+  function handleLoggedIn(){
+    setIsLoggedIn( (isLoggedIn) => !isLoggedIn);
+  }
+
+ 
+  useEffect(() => {
+      fetch(`http://localhost:3000/users`)
+        .then(r => r.json())
+        .then(data => setUser(data))
+        .catch(error => alert(error));
+  }, []);
+  
+  
   return (
     <div className="app">
-      <Header />
-      <NoteContainer />
+      <Header 
+      isLoggedIn={isLoggedIn}
+      handleLoggedIn={handleLoggedIn}
+      user={user}
+      />
+      <NoteContainer isLoggedIn={isLoggedIn}/>
     </div>
   );
 }
