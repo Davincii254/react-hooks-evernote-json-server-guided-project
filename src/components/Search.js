3f43ec1f4bcb901a1a8e41308673f13be74bf1e2
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-04-07 12:30:34 -0500

    Change listener in sort input

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,7 +6,7 @@ function Search({search, handleSearch, sortAlpha, handleSortAlpha}) {
     <div className="filter">
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" name="sort" checked={sortAlpha} onClick={handleSortAlpha}/>
+        <input type="radio" value="Alphabetically" name="sort" checked={sortAlpha} onChange={handleSortAlpha}/>
         Alphabetically
       </label>
       <input id="search-bar" type="text" placeholder="Search Notes" value={search} onChange={handleSearch}/>
