568dddd58bb06c68f7735a7b3f456aeeaf1df4ce
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-04-07 12:44:33 -0500

    Pass state and handler for login to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 import React from "react";
 
-function Header() {
+function Header({isLoggedIn, handleLoggedIn, user}) {
+
   return (
-    <div className="nav-bar">
-      <ul>
-        <li className="nav-item">
-          <h2>Ryan's Note Collection</h2>
-        </li>
-      </ul>
+    <div>
+      <button onClick={handleLoggedIn}>{isLoggedIn ? "Logout" : "Login"}</button>
+      <div className="nav-bar">
+        <ul>
+          <li className="nav-item">
+            <h2>{isLoggedIn ? `${user[0].name.charAt(0).toUpperCase() + user[0].name.slice(1, user[0].name.length - 1)}'s Note Collection` 
+            : "Welcome, Please Login"}
+            </h2>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
