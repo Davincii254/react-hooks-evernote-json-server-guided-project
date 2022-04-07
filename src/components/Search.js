590070d62c6f2aff2f3eee8c0b8df7c91afb3896
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-04-07 11:06:30 -0500

    Add input for sorting and functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-function Search({search, handleSearch}) {
+function Search({search, handleSearch, sortAlpha, handleSortAlpha}) {
 
   return (
     <div className="filter">
+      <strong>Sort by:</strong>
+      <label>
+        <input type="radio" value="Alphabetically" name="sort" checked={sortAlpha} onClick={handleSortAlpha}/>
+        Alphabetically
+      </label>
       <input id="search-bar" type="text" placeholder="Search Notes" value={search} onChange={handleSearch}/>
+
     </div>
   );
 }
