0a814fa94eccca081762ad16e87d96b838ba96fa
Davincii254 <victorkuria7@gmail.com>
2022-04-20 15:26:01 +0300

    update

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,13 +3,7 @@ import NoteEditor from "./NoteEditor";
 import NoteViewer from "./NoteViewer";
 import Instructions from "./Instructions";
 
-/*
-  Advice: If you cannot figure out how to get this component to work,
-          move the div and getContent up into NoteContainer and
-          try to get it to work in the parent first.
-          Then complete the rest of your app before attempting to
-          refactor to get this Content component to work.
-*/
+
 function Content({showItem, handleShow, handleUpdateList, handleDeleteItem}) {
 
   const [editBool, setEditBool] = useState(false);
